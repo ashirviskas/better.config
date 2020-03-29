895e5d44063cca99f9fe87298cf18f2c30a1b313
Minematas <minematas@gmail.com>
2020-03-29 04:39:40 +0300

    basic ckb-next config

---
+++ ckb-next/network_status.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+
+while true
+do
+    ping -c1 1.1.1.1 &>/dev/null\
+        && echo "rgb 00ff00ff" > /tmp/ckbpipe$1\
+        || echo "rgb ff0000ff" > /tmp/ckbpipe$1;
+    sleep 0.3;
+done;
